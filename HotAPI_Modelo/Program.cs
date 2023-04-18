772536684defc7df8ef38bdf7ac4196ed0419c97
Marcos Paulo Ferreira Rebello <marcos.rebello@hotmail.com>
2023-04-18 12:42:07 -0300

    atualizações

--- HotAPI_Modelo/Program.cs
+++ HotAPI_Modelo/Program.cs
@@ -3,7 +3,8 @@ namespace HotAPI_Modelo;
 /// <summary>
 /// Classe de programa padrão para criação da aplicação.
 /// As configurações do 'builder' e 'app' estão no appsettings.json, que
-/// é incorporado no executável da aplicação.
+/// é incorporado no executável da aplicação. Configurações padrão estão
+/// incluídas nas DLLs das bibliotecas (Hot.DLL e HotAPI.DLL)
 /// </summary>
 public class Program : HotAPIServer {
     /// <summary>
