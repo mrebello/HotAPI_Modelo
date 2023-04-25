d66d3c6fa6217f127411d6bc29150ecc05b95af5
Marcos Paulo Ferreira Rebello <marcos.rebello@hotmail.com>
2023-04-25 16:44:00 -0300

    Atualização da HotAPI

--- HotAPI_Modelo/HotGlobals.cs
+++ HotAPI_Modelo/HotGlobals.cs
@@ -1,9 +1,6 @@
 ﻿global using Hot;
 global using Hot.Extensions;
-global using Hot.Extensions.HotLogExtensions;
-global using Hot.Loggers;
 
 global using static Hot.Functions;
-global using static Hot.HotConfiguration;
 global using static Hot.HotConfiguration.config;
 global using static Hot.HotLog.log;
