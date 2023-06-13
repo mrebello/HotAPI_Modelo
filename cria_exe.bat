964144bd5efdf0dcc5032f517c4908fb7347b116
Marcos Paulo Ferreira Rebello <marcos.rebello@hotmail.com>
2023-06-13 11:27:32 -0300

    .net8 + razor

--- cria_exe.bat
+++ cria_exe.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a HotAPI_Modelo.7z HotAPI_Modelo.zip
+"C:\Program Files\7-Zip-Zstandard\7z.exe" a HotAPI_Modelo.7z HotAPI_Modelo.zip
 copy /b 7zsd_All_x64.sfx + config.txt + HotAPI_Modelo.7z HotAPI_Modelo_install.exe
